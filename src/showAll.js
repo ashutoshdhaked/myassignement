0eebb5e9b218ca200b8a636506a93cbda905d482
unknown <ashutoshdhakad375@gmail.com>
2023-11-30 16:14:34 +0530

    first commit

--- src/showAll.js
+++ src/showAll.js
@@ -2,12 +2,8 @@ const ShowAll = ()=>{
     return (
         <div>
             Showing all the Students Data 
-
-
-            here we are making some changes 
-
-
-            It is only Show in The Second User Branch Nothing will Display in Main 
+               this the user branch
+               ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,, 
         </div>
     )
 }
